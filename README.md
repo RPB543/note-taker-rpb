0c2a3f345c4109564610860a5a50f1f5ce721e6e
Becca Byrne <aggiebec06@gmail.com>
2021-12-17 16:07:12 -0600

    updated api routes

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ## Objective
 
+The objective of the challenge is to utilize the provided front end and formatting documents and generate the back end files to successfuly create a note taking application.  This application will use an Express.js back end and will save and retrieve note data from a JSON file.  Once the files have been generated and connected, the application will be deployed to Heroku.
+
 ## Acceptance Criteria
 
 * 
 
-## Built With:
+## Tools Used:
 
 * Javascript
 * CSS
@@ -14,4 +16,4 @@
 * Node JS
 * Node packages
     * Express
-    * 
+* Heroku
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,20 +2,32 @@ const express = require('express');
 const fs = require("fs");
 const router = express.Router();
 const path = require('path');
+const util = require('util');
+const { v4:uuidv4} = require("uuid");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+function readNotes() {
+    return readFile("db/db.json", "utf-8");
+}
 
-
-router.get('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, "../db/db.json"), (err, data) => {
-    if (err) throw err;
-
-    const notes = JSON.parse(data);
-    res.json(notes);
-  })
-});
-
-// router.post('/notes', (req, res) => {
-//   
-// });
+router.get("/notes",(req,res) => {
+  readNotes().then(data => {
+      let notes= [];
+      notes = notes.concat(JSON.parse(data));
+      return notes
+  }).then(notes => res.json(notes)).catch(err => res.json(err));
+})
+router.post("/notes", (req, res)=> {
+  var newNote = {title:req.body.title, text:req.body.text, id:uuidv4()};
+  readNotes().then(data => {
+      let notes= [];
+      notes = notes.concat(JSON.parse(data))
+      notes.push(newNote)
+      return notes
+  }).then(notes => {
+      return writeFile("db/db.json", JSON.stringify(notes));
+  }).then(response => res.send({msg: "Success"}));
+})
 
 // router.delete('/notes:id', (req, res) => {
 //   
